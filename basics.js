51f3dd2976ba84e1ad285082cdd44f1858e200fa
Souvik Naskar <souvik.success@outlook.com>
2023-01-23 13:49:01 +0530

    basic loops in js edit1

--- basics.js
+++ basics.js
@@ -1,58 +1,37 @@
-// js arrays:
-//2 ways of creating an array
-const colors = ["red", "green", "yellow", "violet", "pink", "white", "black"];
+const flowers = ["Dalia", "Marigold", "Tulip", "Rose", "Lily", "Orchid"]
 
-const bikes = new Array("Honda", "Suzuki", "Hero", "Jawa", "Yamaha");
+console.log("------------FOR OF LOOP--------------");
 
-//push, pop, shift, unshift
+for (let flower of flowers){
+    console.log (`${flower}`);
+}
 
-const mobiles = ['Samsung','Motorola', 'Apple', 'Xiaomi'];
+console.log("------------WHILE LOOP--------------");
 
-mobiles.push('Realme', 'OnePlus'); // push 2 elements at the end of an array
+let fl_count = 0;
+while(fl_count < flowers.length){
+    console.log(flowers[fl_count]);
+    fl_count+=1;
+}
 
-console.log(mobiles);
+console.log("------------SIMPLE FOR LOOP--------------");
 
-let popped_el = mobiles.pop(); //pop last element and returns it
-console.log(popped_el.toLowerCase());
-console.log(mobiles);
+for(let i= 0; i<= 30; i+=5){
+    console.log(i);
+}
 
-const birds = new Array(); //initializing an empty array
+console.log("------------FOR IN LOOP--------------");
 
-//unshift
-birds.unshift("Parrot", "Canary"); //inserts two elements in order, start of an array
-console.log(birds);
-birds.unshift("Cuckoo", "Crow", "Falcon", "Hummingbird");
-console.log(birds);
+const car = {type:"Fiat", model:"500", color:"white", drive: "2WD", year:"2016"};
 
+for(let prop in car){
+    console.log(`${prop.toUpperCase()} --> ${car[prop]}`);
+}
 
-//shift
-let myBird = birds.shift(); //removes first element of an array and returns it  
-console.log(myBird.toUpperCase());
-//----------------------------------------
-//splice ---> changes the original arrray, add/remove elements at any given index
+//same thing using Object.keys(obj_name):
 
-const bands = ["Beatles", "GnR", "Queen", "Aerosmith"];
-//arr.splice(index, no_of_del_items, insert_items)
-bands.splice(2,0,"PinkFloyd", "AC/DC"); //insert 2 bands starting at index 2
-console.log(`all bands = ${bands}`);
+for(let pr of Object.keys(car)){ //notice using "FOR OF" loop
+    console.log(`${pr} --> ${car[pr]}`);
+}
 
-const myBands = bands.splice(1,3,"U2"); //returns 3 bands starting from index 1, insert U2 at index 1
-console.log(`removed bands = ${myBands}`);
-console.log(`new bands = ${bands}`);
-//----------------------------------------
-//slice
-const fruits = ["apple", "kiwi", "mango", "watermelon", "orange"];
-let fruitsCopy = fruits.slice(); //creates a copy of the fruits
-
-console.log(fruits);
-
-const myFruits1 = fruitsCopy.slice(2,4);
-console.log(myFruits1);
-
-const myFruits2 = fruitsCopy.slice(-2) ; //gets second last element and last element
-console.log(myFruits2);
-
-let fruit_rem = fruitsCopy.pop();
-console.log(fruitsCopy);
-console.log(fruits);
-
+//Object.values(obj_name) returns values
